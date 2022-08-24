a810d4a4feabb8f56821ed7baa6ce0651c5e2be5
gabribeta <104129009+gabribeta@users.noreply.github.com>
2022-08-24 19:47:41 +0200

    Add files via upload

---
+++ Dog year.js
@@ -0,0 +1,17 @@
+// My age
+const myAge = 19;
+//Early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Gabriel".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in human years which is DOG AGE years old in dog years.`);
---
+++ Kelvin Weather.js
@@ -0,0 +1,12 @@
+const kelvin = 293;
+// Esto son kelvin...
+const celsius = - 273;
+// Esto son celsius...
+let fahrenheit = celsius * (9 / 5) + 32;
+//Ecuación empleada para calcular Fahrenheit
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature ${fahrenheit} degrees Fahrenheit.`);
+let newton = celsius * (33 / 100);
+newton = Math.floor(newton);
+console.log(`The temperature ${newton} degrees Newton.`)
+
